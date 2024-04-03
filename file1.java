7325e5b0949c17920941667397416eb3643a3cd3
vasavichintalapudi <159253065+vasavichintalapudi@users.noreply.github.com>
2024-04-03 17:01:54 +0530

    Update file1.java

--- file1.java
+++ file1.java
@@ -1,20 +1,20 @@
 public class DuplicateWord {    
     public static void main(String[] args) {    
         String string = "This is a string is to find duplicates";    
-        int count;        
+        int c;        
         string = string.toLowerCase();        
-        String words[] = string.split(" ");       
+        String w[] = string.split(" ");       
         System.out.println("Duplicate words in a given string : ");     
-        for(int i = 0; i < words.length; i++) {    
-            count = 1;    
-            for(int j = i+1; j < words.length; j++) {    
-                if(words[i].equals(words[j])) {    
-                    count++;       
-                    words[j] = "0";    
+        for(int i = 0; i < w.length; i++) {    
+            c = 1;    
+            for(int j = i+1; j < w.length; j++) {    
+                if(w[i].equals(w[j])) {    
+                    c++;       
+                    w[j] = "0";    
                 }    
             }       
-            if(count > 1 && words[i] != "0")    
-                System.out.println(words[i]);    
+            if(c > 1 && w[i] != "0")    
+                System.out.println(w[i]);    
         }    
     }    
 } 
